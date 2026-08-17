6ef76319ceacae5a44b47025f17f0ce8786aedc8
Akash Tiwari <shounaktiwari@gmail.com>
2026-08-17 15:00:45 +0530

    17/08/2026

--- ProjectName/ProjectName/urls.py
+++ ProjectName/ProjectName/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from app.views import index
+from app.views import index,get_index,update_information,delete_information
 
 urlpatterns = [
     path('',index),
+    path('get/',get_index),
+    path('update/<int:id>',update_information),
+    path('delete/<int:id>',delete_information)
 ]
 
 # path is an function of urls which take views as an arguments 
--- ProjectName/app/views.py
+++ ProjectName/app/views.py
@@ -13,6 +13,7 @@ from rest_framework.decorators import api_view
 from .serializer import InfoSerializers
 from rest_framework.response import Response
 from rest_framework.status import * 
+from .models import Info
 
 
 @api_view(['POST'])
@@ -31,4 +32,60 @@ def index(request):
                 "message":"invalid data",
                 "code" : HTTP_406_NOT_ACCEPTABLE
             })
-        
+
+# get apis 
+@api_view(['GET'])
+def get_index(request):
+       data = Info.objects.all()
+       serializer = InfoSerializers(data,many=True)
+
+       return Response({
+              "status":True,
+              "message":"Data Fetched Successfully",
+              "data":serializer.data,
+              "code":HTTP_200_OK
+       })
+# update information :perform changes in existing data 
+@api_view(['PUT'])
+def update_information(request,id):
+       try:
+              data = Info.objects.get(id=id)
+       except Info.DoesNotExist:
+              return Response({
+                     "status":False,
+                     "message":"data not found",
+                     "code":HTTP_404_NOT_FOUND
+              })
+       serializer = InfoSerializers(data,data = request.data)
+       if serializer.is_valid():
+              serializer.save()
+              return Response({
+                     "status":True,
+                     "message" : "data updated successfully",
+                     "code":HTTP_200_OK
+              })
+       else:
+            return Response({
+                   "status":False,
+                   "message":"Invalid data",
+                   "error":serializer.error,
+                   "code":HTTP_400_BAD_REQUEST
+            })
+       
+#delete : delete existing info in db 
+@api_view(['DELETE'])
+def delete_information(request,id):
+       try:
+              data = Info.objects.get(id=id)
+       except Info.DoesNotExist:
+              return Response({
+                     "status":False,
+                     "Message":"data not found",
+                     "code":HTTP_404_NOT_FOUND
+              })
+       data.delete()
+       return Response({
+              "status":True,
+              "message":"data deleted successfully",
+              "code":HTTP_200_OK
+       })
